e526dfeb4a21a511d56aa41d7e6ce8ec8e6124fe
Piyush Deshmukh <piyushd2112@gmail.com>
2023-12-29 14:09:10 +0530

    remove unused imports

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,6 +1,6 @@
 import { FormRow, FormRowSelect, SubmitBtn } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 import { Form, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
